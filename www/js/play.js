ec2b3f6422fc001bc28ba60b7660869b9a07992e
Josh Bumgarner <bobulousness@gmail.com>
2016-03-05 11:34:13 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -8,25 +8,26 @@ Dodge.Play.prototype = {
   },
   preload:function (){
     this.load.image('background', 'assets/ocstrm.png');
-    this.load.spritesheet('obj', 'assets/blbrd.png', 31, 64, 4);
-    this.load.spritesheet('player', 'assets/arcr.png', 96, 64, 23);
+    this.load.spritesheet('obj', 'assets/blbrd.png', 31, 67, 1);
+    this.load.spritesheet('player', 'assets/arcr.png', 84, 51, 23);
   },
 
   create: function () {
     this.background = this.add.tileSprite(0,0,320,568,'background');
     this.background.autoScroll(50,0);
     this.background.scale.set(1);
-
     //obj
-    this.obj = this.add.sprite(31,64,'obj');
-    this.obj.anchor.setTo(.5, .5);
+    this.obj = this.add.sprite(31,67,'obj');
+    this.obj.anchor.setTo(0.5, 0.5);
+    /*
     this.obj.animations.add('blink');
-    this.animations.play('blink', 2, true);
+    this.obj.animations.play('blink', 2, true);
+    */
 
     //player
-    this.player = this.add.sprite(100,500,'obj');
-    this.player.anchor.setTo(.0, .5);
-    this.player.animationis.add('blink');
+    this.player = this.add.sprite(100,500,'player');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('blink');
     this.player.animations.play('blink', 2, true);
 
     //keys
@@ -36,7 +37,7 @@ Dodge.Play.prototype = {
   update: function () {
     if (this.cursors.left.isDown){
       this.player.x -= 4;
-    };
+    }
     if (this.cursors.right.isDown) {
       this.player.x += 5;
     };
