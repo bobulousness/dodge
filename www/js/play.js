99eeb2c4d71647c50cd09816ab15320ec1af9c1e
Josh Bumgarner <bobulousness@gmail.com>
2016-03-05 11:06:13 -0500

    save

---
+++ www/js/play.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var Dodge = {};
+Dodge.Play = function () {};
+Dodge.Play.prototype = {
+  init: function() {
+    console.log("%c--- booting ---\n compliments of urmum");
+  },
+  preload:function (){
+    this.load.image('background', 'assets/ocstrm.png');
+    this.load.spritesheet
+  },
+
+  create: function () {
+    this.background = this.add.tileSprite(0,0,320,560,'ocstrm');
+    this.background.autoScroll(50,0);
+    this.background.scale.set(1);
+
+    //onj
+    this.obj = this.add.sprite(31,64,'obj');
+    this.obj.anchor.setTo(.5, .5);
+    this.obj.animations.add('blink');
+    this.animations.play('blink', 2, true);
+
+    //player
+    this.player = this.add.sprite(100,500,'obj');
+    this.player.anchor.setTo(.0, .5);
+    this.player.animation.add('blink');
+    this.player.animations.play('blink', 2, true);
+
+    //keys
+    this.cursors = game.input.keyboard.createCursorKeys();
+  },
+
+  update: function () {
+    if (this.cursors.left.isDown){
+      this.player.x -= 4;
+    };
+    if (this.cursors.right.isDown) {
+      this.player.x += 5;
+    };
+  }
+
+};
+
