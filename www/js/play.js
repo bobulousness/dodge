fa548b4ae2fbb1a7051f6b9453333ca7f0e73278
Josh Bumgarner <bobulousness@gmail.com>
2016-03-19 11:58:53 -0400

    save

--- www/js/play.js
+++ www/js/play.js
@@ -18,12 +18,18 @@ Dodge.Play.prototype = {
   },
 
   create: function () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //background
     this.background = this.add.tileSprite(0,0,320,568,'background');
     this.background.autoScroll(50,0);
     this.background.scale.set(1);
+
     //obj
     this.obj = this.add.sprite(31,67,'obj');
     this.obj.anchor.setTo(0.5, 0.5);
+    game.physics.arcade.enable(this.obj);
+    this.body.velocity.y = 10
     /*
     this.obj.animations.add('blink');
     this.obj.animations.play('blink', 2, true);
@@ -34,19 +40,19 @@ Dodge.Play.prototype = {
     this.player.anchor.setTo(0.5, 0.5);
     this.player.animations.add('blink');
     this.player.animations.play('blink', 2, true);
+    game.physics.arcade.enable(this.player);
+    this.player.body.collideWorldBounds = true
+    this.player.body.bounce.setTo(0.3);
+    this.player.body.drag.setTo(3000);
 
     //keys
     this.cursors = game.input.keyboard.createCursorKeys();
   },
 
   update: function () {
-
-    p
-    if (this.player.x < 1
-    //obj falling
-    this.obj.y += 10;
+       
     if ( this.obj.y > 400){
-      this.obj.y = 10;
+      this.obj.y = 10
       this.obj.x = game.rnd.integerInRange(31,289);
      
     }
@@ -71,7 +77,6 @@ Dodge.Play.prototype = {
     //player movement
     if (this.cursors.left.isDown){
       this.player.x -= 4;
-      p
     }
     if (this.cursors.right.isDown) {
       this.player.x += 5;
